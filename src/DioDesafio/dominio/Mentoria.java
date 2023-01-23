7c3f20a14f31dc34936e26b40a81857c80dc387a
figueredobeth <lizaefigueredo@gmail.com>
2023-01-22 22:57:11 -0300

    Desafio Dio Programacao Orientada a Objeto git status

---
+++ src/DioDesafio/dominio/Mentoria.java
@@ -0,0 +1,35 @@
+package DioDesafio.dominio;
+
+import java.time.LocalDate;
+
+public abstract class Mentoria extends Conteudo{
+
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData (LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "título='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+}
